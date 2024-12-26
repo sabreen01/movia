323220823aaa036f96ad49caac3e938718bd5679
Farah <sabreenessam2025@gmail.com>
2024-12-26 23:25:39 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+import { validations } from "./formValidtion.js";
+import { openNav } from "./sideNave.js";
+import { getMovie, searchMovie } from "./moviesData.js";
+import { getMovieAttr, navGetSection, scroll, topZero } from "./scroll.js";
+validations();
+scroll();
+getMovie("movie/now_playing");
+$(".nav-menu").click(openNav);
+$(".menu a").click(getMovieAttr);
+$(".menu li a").click(navGetSection);
+$("#back-to-top").click(topZero);
+$("#search").on("input", (e) => {
+  searchMovie(e.target.value);
+  if (e.target.value == "") {
+    getMovie("movie/now_playing");
+  }
+});
+$(document).ready(function () {
+  $(".loading").fadeOut(2000);
+});
